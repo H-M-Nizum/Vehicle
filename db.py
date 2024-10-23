1955893e4e8eaac8b383af33316b52a39f63ce33
H-M-Nizum <hmnizum1714032@gmail.com>
2024-10-23 17:35:54 +0600

    Requerment.txt and update db for instand use

--- db.py
+++ db.py
@@ -1,22 +1,29 @@
-import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+client = MongoClient(f"mongodb+srv://nizum:5kXPMsQBqhf65LZr@cluster0.4fphg1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+vehicle_conn = client['Vehicle']
+allocation_conn = client['Allocation']
 
-# Get USERNAMES and password from environment variables
-USERNAMES = os.getenv('USERNAMES')
-PASSWORD = os.getenv('PASSWORD')
 
-print('Loaded USERNAMES:', USERNAMES)  # Debug print
-print('Loaded PASSWORD:', PASSWORD)  # Debug print
+# import os
+# from pymongo import MongoClient
+# from dotenv import load_dotenv
 
-# Create MongoDB client using the environment variables
-if USERNAMES and PASSWORD:
-    client = MongoClient(f"mongodb+srv://{USERNAMES}:{PASSWORD}@cluster0.4fphg1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    vehicle_conn = client['Vehicle']
-    allocation_conn = client['Allocation']
-    print('MongoDB client created successfully.')
-else:
-    print('Error: USERNAMES or PASSWORD not loaded from .env file.')
+# # Load environment variables from .env file
+# load_dotenv()
+
+# # Get USERNAMES and password from environment variables
+# USERNAMES = os.getenv('USERNAMES')
+# PASSWORD = os.getenv('PASSWORD')
+
+# print('Loaded USERNAMES:', USERNAMES)  # Debug print
+# print('Loaded PASSWORD:', PASSWORD)  # Debug print
+
+# # Create MongoDB client using the environment variables
+# if USERNAMES and PASSWORD:
+#     client = MongoClient(f"mongodb+srv://{USERNAMES}:{PASSWORD}@cluster0.4fphg1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#     vehicle_conn = client['Vehicle']
+#     allocation_conn = client['Allocation']
+#     print('MongoDB client created successfully.')
+# else:
+#     print('Error: USERNAMES or PASSWORD not loaded from .env file.')
